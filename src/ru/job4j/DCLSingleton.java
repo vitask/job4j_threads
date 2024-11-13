c7523950ee1729ac20c73f6a4f7e696a542deb3f
vitask <skalenko.v@yandex.ru>
2024-11-13 23:21:07 +0700

    2. Модель памяти Java [#267917]

---
+++ src/ru/job4j/DCLSingleton.java
@@ -0,0 +1,20 @@
+package ru.job4j;
+
+public class DCLSingleton {
+
+    public DCLSingleton() {
+    }
+
+    private static volatile DCLSingleton instance;
+
+    public static DCLSingleton getInstance() {
+        if (instance == null) {
+            synchronized (DCLSingleton.class) {
+                if (instance == null) {
+                    instance = new DCLSingleton();
+                }
+            }
+        }
+        return instance;
+    }
+}
