62a1bd425836e755d01514f35a8093e7c949e695
vitask <skalenko.v@yandex.ru>
2024-12-18 10:49:21 +0700

    3. Денежные переводы AccountStorage [#1104]

---
+++ src/main/java/ru/job4j/cash/AccountStorage.java
@@ -0,0 +1,44 @@
+package ru.job4j.cash;
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.HashMap;
+import java.util.Optional;
+
+@ThreadSafe
+public class AccountStorage {
+    @GuardedBy("this")
+    private final HashMap<Integer, Account> accounts = new HashMap<>();
+
+    public synchronized boolean add(Account account) {
+        return accounts.putIfAbsent(account.id(), account) == null;
+    }
+
+    public synchronized boolean update(Account account) {
+        return accounts.replace(account.id(), account) != null;
+    }
+
+    public synchronized void delete(int id) {
+        accounts.remove(id);
+    }
+
+    public synchronized Optional<Account> getById(int id) {
+        return Optional.ofNullable(accounts.get(id));
+    }
+
+    public synchronized boolean transfer(int fromId, int toId, int amount) {
+        Account from = accounts.get(fromId);
+        Account to = accounts.get(toId);
+        if (from == null || to == null) {
+            return false;
+        }
+        if (from.amount() < amount) {
+            throw new IllegalArgumentException("No money on account");
+        }
+        accounts.put(fromId, new Account(fromId, from.amount() - amount));
+        accounts.put(toId, new Account(toId, to.amount() + amount));
+        return false;
+    }
+}
+
